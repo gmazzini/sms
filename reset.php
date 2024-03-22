1539b9d1445fd24923c7b7d8bb1139c8bb219146
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-22 16:46:13 +0100

    Create reset.php

---
+++ reset.php
@@ -0,0 +1,16 @@
+<?php
+  
+$num=$argv[1];
+
+$fd=dio_open("/dev/ttyCH9344USB".$num,O_RDWR|O_NOCTTY|O_NONBLOCK);
+dio_tcsetattr($fd,array("baud"=>115200,"bits"=>8,"stop"=>1,"parity"=>0,"flow_control"=>0,"is_canonical"=>0));
+sleep(2);
+
+dio_write($fd,"AT+CFUN=1,1\r");
+usleep(1000000);
+$oo=dio_read($fd);
+echo $oo;
+
+dio_close($fd);
+
+?>
