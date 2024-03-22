42c1846fd77eb8ea2baea2ab9e44dd30f4076e6f
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-22 16:42:35 +0100

    Update test_all.php

--- test_all.php
+++ test_all.php
@@ -2,13 +2,14 @@
 
 for($num=0;$num<32;$num++){
   $fd=dio_open("/dev/ttyCH9344USB".$num,O_RDWR|O_NOCTTY|O_NONBLOCK);
+  usleep(100000);
   dio_tcsetattr($fd,array("baud"=>115200,"bits"=>8,"stop"=>1,"parity"=>0,"flow_control"=>0,"is_canonical"=>0));
-  sleep(2);
+  usleep(100000);
   dio_write($fd,"AT+CIMI;+GSN;+CREG?\r");
-  usleep(1000000);
+  usleep(100000);
   $oo=dio_read($fd);
   echo "$num $oo\n\n";
-  sleep(1);
+  usleep(100000);
   dio_close($fd);
 }
 
