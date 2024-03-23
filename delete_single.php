0c4f186241d1f03986d6bb54d0326d3e4e927ff0
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-23 05:47:55 +0100

    Create delete_single.php

---
+++ delete_single.php
@@ -0,0 +1,14 @@
+<?php
+
+$num=$argv[1];
+$fd=dio_open("/dev/ttyCH9344USB".$num,O_RDWR|O_NOCTTY|O_NONBLOCK);
+usleep(100000);
+dio_tcsetattr($fd,array("baud"=>115200,"bits"=>8,"stop"=>1,"parity"=>0,"flow_control"=>0,"is_canonical"=>0));
+usleep(100000);
+dio_write($fd,"AT+CMGD=1,4\r");
+usleep(100000);
+$oo=dio_read($fd);
+echo "$num $oo";
+dio_close($fd);
+
+?>
