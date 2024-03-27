d799f8a99cb3fbcdf7993f18647a7824aa9aa3fc
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-27 05:49:24 +0100

    Create web_index.php

---
+++ web_index.php
@@ -0,0 +1,13 @@
+<?php
+
+include "auth.php";
+if(isset($_GET['to']))$to=$_GET['to'];
+if(isset($_GET['text']))$text=$_GET['text'];
+if(isset($_GET['token']))$token=$_GET['token'];
+if(strlen($to)>10&&strlen($text)>4&&$aaa[$token]==1){
+  $fp=fopen("/home/inout","w+");
+  fwrite($fp,"$to|$text\n");
+  fclose($fp);
+}
+
+?>
