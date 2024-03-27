d70dcf07afde2027e2d52becaca68d36b5a87b4f
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-27 05:53:49 +0100

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,7 +1,7 @@
 <?php
-  
+include "local.php";
 $num=$argv[1];
-$fd=dio_open("/dev/ttyCH9344USB".$num,O_RDWR|O_NOCTTY|O_NONBLOCK);
+$fd=dio_open($mydev.$num,O_RDWR|O_NOCTTY|O_NONBLOCK);
 usleep(100000);
 dio_tcsetattr($fd,array("baud"=>115200,"bits"=>8,"stop"=>1,"parity"=>0,"flow_control"=>0,"is_canonical"=>0));
 usleep(100000);
@@ -10,5 +10,4 @@ usleep(100000);
 $oo=dio_read($fd);
 echo $oo;
 dio_close($fd);
-
 ?>
