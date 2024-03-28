d195512e0eb9d7a0d485dc035f188a7e327cc152
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-28 07:15:11 +0100

    Update test_all.php

--- test_all.php
+++ test_all.php
@@ -2,14 +2,16 @@
 include "local.php";
 for($num=0;$num<32;$num++){
   $fd=dio_open($mydev.$num,O_RDWR|O_NOCTTY|O_NONBLOCK);
-  usleep(100000);
+  usleep(50000);
   dio_tcsetattr($fd,array("baud"=>115200,"bits"=>8,"stop"=>1,"parity"=>0,"flow_control"=>0,"is_canonical"=>0));
-  usleep(100000);
-  dio_write($fd,"AT+CIMI;+GSN;+CREG?\r");
-  usleep(100000);
-  $oo=dio_read($fd);
-  echo "$num $oo\n";
-  usleep(100000);
+  usleep(50000);
+  dio_write($fd,"AT\r");
+  usleep(50000);
+  $oo=trim(dio_read($fd));
+  if(substr($oo,-2)=="OK")echo "$num:OK ";
+  else echo "$num:NO ";
+  usleep(50000);
   dio_close($fd);
 }
+echo "\n";
 ?>
